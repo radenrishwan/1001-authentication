8397b732b3afc520090d68381fbb44b683b30860
radenrishwan <radenrishwan@gmail.com>
2025-01-16 14:33:56 +0700

    implement github oauth2

--- backend/oauth2.go
+++ backend/oauth2.go
@@ -7,6 +7,7 @@ import (
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/facebook"
+	"golang.org/x/oauth2/github"
 	"golang.org/x/oauth2/google"
 )
 
@@ -30,6 +31,10 @@ func (_ Oauth2AuthenticationType) Init() {
 	FACEBOOK_CLIENT_SECRET = os.Getenv("FACEBOOK_CLIENT_SECRET")
 	FACEBOOK_REDIRECT_URL = os.Getenv("FACEBOOK_REDIRECT_URL")
 
+	GITHUB_CLIENT_ID = os.Getenv("GITHUB_CLIENT_ID")
+	GITHUB_CLIENT_SECRET = os.Getenv("GITHUB_CLIENT_SECRET")
+	GITHUB_REDIRECT_URL = os.Getenv("GITHUB_REDIRECT_URL")
+
 	googleOauthConfig = &oauth2.Config{
 		ClientID:     GOOGLE_CLIENT_ID,
 		ClientSecret: GOOGLE_CLIENT_SECRET,
@@ -44,6 +49,14 @@ func (_ Oauth2AuthenticationType) Init() {
 		RedirectURL:  FACEBOOK_REDIRECT_URL,
 		Scopes:       facebook_scopes,
 		Endpoint:     facebook.Endpoint,
+	}
+
+	githubOauthConfig = &oauth2.Config{
+		ClientID:     GITHUB_CLIENT_ID,
+		ClientSecret: GITHUB_CLIENT_SECRET,
+		RedirectURL:  GITHUB_REDIRECT_URL,
+		Scopes:       github_scopes,
+		Endpoint:     github.Endpoint,
 	}
 }
 
@@ -160,6 +173,64 @@ func (_ Oauth2AuthenticationHandler) Bind(mux *http.ServeMux) {
 
 	mux.HandleFunc("/api/protected-facebook-oauth2-data", FacebookOauth2Middleware(func(w http.ResponseWriter, r *http.Request) {
 		user := r.Context().Value("facebook_user").(*FacebookUser)
+		WriteJsonResponse(w, map[string]interface{}{
+			"message": "this is protected data",
+			"user":    user,
+		})
+	}))
+
+	mux.HandleFunc("/api/authentication/login/github", func(w http.ResponseWriter, r *http.Request) {
+		url := githubOauthConfig.AuthCodeURL(oauthState)
+		http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	})
+
+	mux.HandleFunc("/api/authentication/login/github/callback", func(w http.ResponseWriter, r *http.Request) {
+		state := r.URL.Query().Get("state")
+		if state != oauthState {
+			WriteJsonErrorResponseWithStatus(w, "invalid oauth state", http.StatusBadRequest)
+			return
+		}
+
+		code := r.URL.Query().Get("code")
+		if code == "" {
+			WriteJsonErrorResponseWithStatus(w, "code not found", http.StatusBadRequest)
+			return
+		}
+
+		token, err := githubOauthConfig.Exchange(r.Context(), code)
+		if err != nil {
+			WriteJsonErrorResponseWithStatus(w, "failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		client := githubOauthConfig.Client(r.Context(), token)
+		resp, err := client.Get("https://api.github.com/user")
+		if err != nil {
+			WriteJsonErrorResponseWithStatus(w, "failed to get user info: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		var githubUser GitHubUser
+		body, _ := io.ReadAll(resp.Body)
+		if err := githubUser.FromJSON(body); err != nil {
+			WriteJsonErrorResponseWithStatus(w, "failed to parse user info: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		response := struct {
+			User  GitHubUser `json:"user"`
+			Token string     `json:"token"`
+		}{
+			User:  githubUser,
+			Token: token.AccessToken,
+		}
+
+		WriteJsonResponse(w, response)
+	})
+
+	mux.HandleFunc("/api/protected-github-oauth2-data", GitHubOauth2Middleware(func(w http.ResponseWriter, r *http.Request) {
+		user := r.Context().Value("github_user").(*GitHubUser)
 		WriteJsonResponse(w, map[string]interface{}{
 			"message": "this is protected data",
 			"user":    user,
