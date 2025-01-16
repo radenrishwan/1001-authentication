1b78bb7f7e03777b6d8cb63d8052622ac36eb632
radenrishwan <radenrishwan@gmail.com>
2025-01-16 14:15:37 +0700

    implmement facebook & move into google file for google oauth

--- backend/oauth2.go
+++ backend/oauth2.go
@@ -1,57 +1,14 @@
 package authentication
 
 import (
-	"context"
-	"encoding/json"
-	"errors"
 	"io"
 	"net/http"
 	"os"
-	"strings"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/facebook"
 	"golang.org/x/oauth2/google"
 )
-
-var (
-	GOOGLE_CLIENT_ID     string
-	GOOGLE_CLIENT_SECRET string
-	GOOGLE_REDIRECT_URL  string
-	google_scopes        = []string{
-		"https://www.googleapis.com/auth/userinfo.email",
-		"https://www.googleapis.com/auth/userinfo.profile",
-	} // references: https://developers.google.com/identity/protocols/oauth2/scopes
-
-	oauthState = "random" // TODO: change this to random string later
-)
-
-var (
-	FACEBOOK_CLIENT_ID     string
-	FACEBOOK_CLIENT_SECRET string
-	FACEBOOK_REDIRECT_URL  string
-	facebook_scopes        = []string{} // TODO: add later
-)
-
-var googleOauthConfig = &oauth2.Config{}
-
-var facebookOauthConfig = &oauth2.Config{}
-
-type GoogleUser struct {
-	ID            string `json:"id"`
-	Email         string `json:"email"`
-	VerifiedEmail bool   `json:"verified_email"`
-	Name          string `json:"name"`
-	Picture       string `json:"picture"`
-}
-
-func (u *GoogleUser) ToJSON() ([]byte, error) {
-	return json.Marshal(u)
-}
-
-func (u *GoogleUser) FromJSON(data []byte) error {
-	return json.Unmarshal(data, u)
-}
 
 type Oauth2AuthenticationType struct{}
 
@@ -87,60 +44,6 @@ func (_ Oauth2AuthenticationType) Init() {
 		RedirectURL:  FACEBOOK_REDIRECT_URL,
 		Scopes:       facebook_scopes,
 		Endpoint:     facebook.Endpoint,
-	}
-}
-
-func (_ Oauth2AuthenticationType) GoogleVerifyUser(r *http.Request) (*GoogleUser, error) {
-	authHeader := r.Header.Get("Authorization")
-	tokenString := strings.TrimSpace(authHeader[7:])
-
-	token := &oauth2.Token{
-		AccessToken: tokenString,
-		TokenType:   "Bearer",
-	}
-
-	client := googleOauthConfig.Client(r.Context(), token)
-	resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
-	if err != nil {
-		return nil, errors.New("Failed to get user info: " + err.Error())
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New("Invalid or expired token")
-	}
-
-	var googleUser GoogleUser
-	body, _ := io.ReadAll(resp.Body)
-	if err := googleUser.FromJSON(body); err != nil {
-		return nil, errors.New("Failed to decode user info: " + err.Error())
-	}
-
-	return &googleUser, nil
-}
-
-func GoogleOauth2Middleware(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		auth := r.Header.Get("Authorization")
-		if auth == "" {
-			WriteJsonErrorResponseWithStatus(w, "authorization is empty", http.StatusUnauthorized)
-			return
-		}
-
-		if !strings.HasPrefix(auth, "Bearer ") {
-			WriteJsonErrorResponseWithStatus(w, "unsupported auth method", http.StatusUnauthorized)
-			return
-		}
-
-		user, err := Oauth2Authentication.GoogleVerifyUser(r)
-		if err != nil {
-			WriteJsonErrorResponseWithStatus(w, err.Error(), http.StatusUnauthorized)
-			return
-		}
-
-		// store the user in the context
-		ctx := context.WithValue(r.Context(), "user", user)
-		next.ServeHTTP(w, r.WithContext(ctx))
 	}
 }
 
@@ -199,6 +102,64 @@ func (_ Oauth2AuthenticationHandler) Bind(mux *http.ServeMux) {
 
 	mux.HandleFunc("/api/protected-google-oauth2-data", GoogleOauth2Middleware(func(w http.ResponseWriter, r *http.Request) {
 		user := r.Context().Value("user").(*GoogleUser)
+		WriteJsonResponse(w, map[string]interface{}{
+			"message": "this is protected data",
+			"user":    user,
+		})
+	}))
+
+	mux.HandleFunc("/api/authentication/login/facebook", func(w http.ResponseWriter, r *http.Request) {
+		url := facebookOauthConfig.AuthCodeURL(oauthState)
+		http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	})
+
+	mux.HandleFunc("/api/authentication/login/facebook/callback", func(w http.ResponseWriter, r *http.Request) {
+		state := r.URL.Query().Get("state")
+		if state != oauthState {
+			WriteJsonErrorResponseWithStatus(w, "invalid oauth state", http.StatusBadRequest)
+			return
+		}
+
+		code := r.URL.Query().Get("code")
+		if code == "" {
+			WriteJsonErrorResponseWithStatus(w, "code not found", http.StatusBadRequest)
+			return
+		}
+
+		token, err := facebookOauthConfig.Exchange(r.Context(), code)
+		if err != nil {
+			WriteJsonErrorResponseWithStatus(w, "failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		client := facebookOauthConfig.Client(r.Context(), token)
+		resp, err := client.Get("https://graph.facebook.com/v12.0/me?fields=id,name,email,picture")
+		if err != nil {
+			WriteJsonErrorResponseWithStatus(w, "failed to get user info: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		var facebookUser FacebookUser
+		body, _ := io.ReadAll(resp.Body)
+		if err := facebookUser.FromJSON(body); err != nil {
+			WriteJsonErrorResponseWithStatus(w, "failed to parse user info: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		response := struct {
+			User  FacebookUser `json:"user"`
+			Token string       `json:"token"`
+		}{
+			User:  facebookUser,
+			Token: token.AccessToken,
+		}
+
+		WriteJsonResponse(w, response)
+	})
+
+	mux.HandleFunc("/api/protected-facebook-oauth2-data", FacebookOauth2Middleware(func(w http.ResponseWriter, r *http.Request) {
+		user := r.Context().Value("facebook_user").(*FacebookUser)
 		WriteJsonResponse(w, map[string]interface{}{
 			"message": "this is protected data",
 			"user":    user,
